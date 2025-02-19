ebfa3daa0c93dfdc317998c1a98fcb53979ab4bb
AdyaKumarjha <adyakumar8434@gmail.com>
2025-02-19 12:10:21 +0000

    conversion

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,23 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+// we have to avoid this kind of conversion
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+// console.log(undifined == 0);
+// console.log(undifined > 0);
+// console.log(undifined < 0);
+
+// ===
+
+console.log("2" === 2)
