d91a4c2e4d3a436234f22605433a270942f44cbc
AdyaKumarjha <adyakumar8434@gmail.com>
2025-02-19 09:42:24 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,20 @@
+"use strict";//treat all JS code as newer version
+
+// alert(3+3)//we are  nodejs, not browser
+
+let name = "hitesh"
+let age = 18
+let isLoggedIn = false
+
+// number => 2 to power 53
+// bigint =>
+    //String =>"" 
+//boolean =>true/false
+//null =>standalone value
+// undifiend =>
+// symbol => unique
+
+// object
+
+console.log(typeof undefined);//undefined
+console.log(typeof null);//object
