73132e14585a9dfc4bfa9ab4f7895002d5de34a1
AdyaKumarjha <adyakumar8434@gmail.com>
2025-02-21 14:48:20 +0000

    study about while and do-while loop

---
+++ 05_iterations/two.js
@@ -0,0 +1,12 @@
+
+let index = 0
+while (index <= 10) {
+      console.log(`Value of index is ${index}`);
+      index = index + 2
+}
+
+let score = 30
+do {
+      console.log(`the score is ${score}`);
+      score++
+} while (score <= 20);
