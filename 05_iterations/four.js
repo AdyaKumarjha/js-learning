cddb47fab5a38768722e9d300141e5ca7c1ce33b
AdyaKumarjha <adyakumar8434@gmail.com>
2025-02-22 03:01:44 +0000

    learn about for in loop

---
+++ 05_iterations/four.js
@@ -0,0 +1,26 @@
+const myObject = {
+    js: 'javascript',
+    cpp: 'C++',
+    rb: 'ruby'
+}
+
+for (const key in myObject) {
+    //  console.log(myObject[key]);
+    // console.log(`${key} shortcut is ${myObject[key]}`);   
+}
+
+const programing = ["js", "rb", "py", "java", "cpp"]
+
+for (const key in programing) {
+    //  console.log(programing[key]);    
+}
+
+
+// const map = new Map()
+// map.set("IN", "India")
+// map.set("USA", "United states of America")
+// map.set("FR", "France")
+
+// for (const key in map) {
+//     console.log(key);  
+// }
